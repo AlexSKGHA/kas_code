c4a819f7d99481ccb5cd56da69ff01f71b22038b
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2020-06-01 23:21:36 -0700

    Update PasswordGenerator.java

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -9,64 +9,26 @@ public class PasswordGenerator {
             "0123456789"
     };
 
-    private static byte requiredArrayAmount = 4;
-
-    private Random random = new Random();
+    private final Random random = new Random();
 
     public String make(byte length, boolean[] args) {
+        StringBuilder password = new StringBuilder();
+        StringBuilder symbolsToUse = new StringBuilder();
 
-        boolean validArray = checkArr(args, requiredArrayAmount);
-        boolean validLength = checkPasswordLengthVariable(length);
-
-        if ((!validArray) || (!validLength)) {
-            System.err.println("Invalid arguments");
-            return "Invalid arguments";
-        }
-
-        String password = "";
-        String selectedSymbols = "";
         byte symbolLength = (byte) symbols.length;
 
         for (byte i = 0; i < symbolLength; i++) {
-            if (args[i] == true) {
-                selectedSymbols += symbols[i];
+            if (args[i]) {
+                symbolsToUse.append(symbols[i]);
             }
         }
 
-        byte selectedSymbolsLength = (byte) selectedSymbols.length();
+        byte selectedSymbolsLength = (byte) symbolsToUse.length();
 
         for (int i = 0; i < length; i++) {
-            password += String.valueOf(selectedSymbols.charAt(random.nextInt(selectedSymbolsLength)));
+            password.append(symbolsToUse.charAt(random.nextInt(selectedSymbolsLength)));
         }
 
-        return password;
-    }
-
-    private boolean checkPasswordLengthVariable (byte length) {
-        if (length > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private boolean checkArr (boolean[] args, byte requiredAmount) {
-
-        boolean valueCheck = false;
-        boolean amountCheck = false;
-
-        for (boolean value : args) {
-            if (value == true)
-                valueCheck = true;
-        }
-
-        if (args.length == requiredAmount)
-            amountCheck = true;
-
-        if ((valueCheck == true) && (amountCheck == true)) {
-            return true;
-        } else {
-            return false;
-        }
+        return password.toString();
     }
 }
