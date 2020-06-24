1933bcc9e7c22bb2664699ffabc097a796d0eb03
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2020-06-24 01:14:47 -0700

    Commented getNunOfDigits methods. package kasco...
    Commented getNunOfDigits methods. package kascode statement is now present.

--- NumberParser.java
+++ NumberParser.java
@@ -1,3 +1,5 @@
+package kascode;
+
 public class NumberParser {
 
     public static int[] split (int number) {
@@ -25,19 +27,32 @@ public class NumberParser {
 
     public static int getNumOfDigits (int number,
                                       boolean addMinusToNum) {
+        //represents number of digits of the number argument.
         int num = 0;
 
+        //checks if the number is negative.
         if (number < 0) {
+            //minus as a digit to num variable if addMinusToNum is
+            //true.
             if (addMinusToNum) {
                 num++;
             }
         }
 
+        //if number less than 10 or greater that -10 then
+        //this if clause returns num variable adding one to it
+        //because this number consist exactly of one digit.
+        //if addMinusToNum would be true than minus is considered as
+        //a digit.
         if ((number < 10) && (number > -10)) {
             return ++num;
         }
 
+        //this block of code removes each left digit from
+        //the number variable and adds one to num each iteration.
         while (number != 0) {
+            //this expression removes a last digit of the number
+            //each iteration.
             number = (number - (number % 10)) / 10;
             num += 1;
         }
@@ -45,6 +60,7 @@ public class NumberParser {
         return num;
     }
 
+    //shortcut of getNumOfDigits of 2 parameters.
     public static int getNumOfDigits (int number) {
         return getNumOfDigits(number, true);
     }
