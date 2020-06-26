5a3252168524153335f7d7530e4af5ab10dd4225
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2020-06-26 01:19:26 -0700

    kascode package added & the class fixed
    Before this commit method toInt used to perform wrong way. It could've return 0 when negative value of type long passed.
    Now it can make any long number of int type if this number is in the int number range.

--- PrimitiveTypeParsing.java
+++ PrimitiveTypeParsing.java
@@ -1,14 +1,28 @@
+package kascode;
+
 public class PrimitiveTypeParsing {
-    
+
     public static int toInt (long number) {
-        if (Math.abs(number) < 2147483648L) {
-            int count = 0;
-            for (long i = 0; i < number; i++) {
-                count++;
+
+        if (isLongNumberOfIntRange(number)) {
+
+            int increment;
+            if (number >= 0) {
+                increment = 1;
+            } else {
+                increment = -1;
             }
-            return count;
+            int i = 0;
+            do {
+                i += increment;
+            } while (i != number);
+            return i;
         } else {
             return 0;
         }
+    }
+
+    public static boolean isLongNumberOfIntRange(long num) {
+        return (num >= -2147483648L) && (num <= 2147483647L);
     }
 }
